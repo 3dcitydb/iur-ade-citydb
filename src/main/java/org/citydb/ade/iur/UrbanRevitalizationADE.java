14a0a52822fdf16e35edadcfb6d89d5bab515743
Claus Nagel <cnagel@virtualcitysystems.de>
2020-08-07 19:47:37 +0200

    fixed starter

--- src/main/java/org/citydb/ade/iur/UrbanRevitalizationADE.java
+++ src/main/java/org/citydb/ade/iur/UrbanRevitalizationADE.java
@@ -54,7 +54,7 @@ public class UrbanRevitalizationADE extends ADEExtension implements ADEKmlExport
 
     public static void main(String[] args) {
         UrbanRevitalizationADE adeExtension = new UrbanRevitalizationADE();
-        adeExtension.setBasePath(Paths.get("resources").toAbsolutePath());
+        adeExtension.setBasePath(Paths.get("resources", "database").toAbsolutePath());
         new ImpExp().doMain(args, adeExtension);
     }
 
